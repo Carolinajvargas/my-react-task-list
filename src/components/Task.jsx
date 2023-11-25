09835cbd311b14f2aff53ffcf0784a68cf5f38ac
Carolina Jimenez Vargas. <carolinajimenez020@gmail.com>
2023-11-25 13:38:31 -0500

    .

---
+++ src/components/Task.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+export function Task (props){
+    const { currentTask, currentDescription, currentId, onHandleEdit, onHandleDeleteTask } = props;
+    const [editing, setEditing] = useState(false);
+    const [displayedTask, setDisplayedTask] = useState(currentTask);
+    const [displayedDescription, setDisplayedDescription] = useState(currentDescription);
+
+    let viewMode= {};
+    let editMode= {};
+
+    if (editing){
+        viewMode.display = "none";
+    } else {
+        editMode.display = "none";
+    }
+
+    return (
+    <div className="task">
+        <div>
+            <input type="radio" />
+            <label htmlFor="" style={viewMode}>{ currentTask } </label>
+            <input type="text" value={displayedTask} style={editMode} onChange={(e) => {
+                setDisplayedTask(e.target.value);
+            }}/>
+            <button onClick={() =>{
+                setEditing(true);
+            }} style={viewMode}>✍️</button>
+            <button onClick={() => {
+                onHandleEdit(currentId, displayedTask, displayedDescription);
+                setEditing(false);
+            }} style={editMode}>👍</button>
+            <button onClick={() => {
+                setEditing(false);
+                setDisplayedTask(currentTask);
+                setDisplayedDescription(currentDescription);
+            }} style={editMode}>👎</button>
+            <button onClick={() =>{
+                onHandleDeleteTask(currentId);
+            }} style={viewMode}>🗑️</button>
+        </div>
+        <p style={viewMode}>{ currentDescription }</p>
+        <input type="text" value={displayedDescription} style={editMode} onChange={(e) => {
+            setDisplayedDescription(e.target.value)
+        }}/>
+    </div>
+    );
+};
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,21 @@
+import { Task } from "./Task"
+
+export function TaskList(props) {
+    const { list, onHandleDelete, onHandleEdit, onHandleDeleteTask } = props;
+
+    return (
+        <div>
+            <div className="taskList">
+                {list.map((task) => (
+                    <div key={task.id}>
+                        <Task currentTask={task.title} currentDescription={task.description} currentId={task.id} onHandleEdit={onHandleEdit} onHandleDeleteTask={onHandleDeleteTask} />
+                    </div>
+                ))}
+            </div>
+            <div className="pendingClean">
+                <label htmlFor="">Tiene {list.length} tareas pendientes </label>
+                <button id="cleanAllBttn" onClick={() => onHandleDelete()}>Limpiar tareas</button>
+            </div>
+        </div>
+    );
+};
