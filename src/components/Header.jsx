14cb959bba6eb6f59436493043a4d22a0952d99d
Carolina Jimenez Vargas. <carolinajimenez020@gmail.com>
2023-11-28 21:07:05 -0500

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,68 @@
-export function Header(props) {
-    const { name, onHandleClick, onHandleChangeTitle, onHandleChangeDescription, taskTitle, taskDescription } = props;
+import { Heading, Button, Flex, Stack, Box } from "@chakra-ui/react";
 
-    return (
-        <>
-            <nav>
-                <div className="header">
-                    <h1>{name}</h1>
-                </div>
-            </nav>
-            <div className="newTask">
-                <h3>Nueva tarea:</h3>
-                <form onSubmit={(e) => {
-                    e.preventDefault();
-                    onHandleClick();
-                }}>
-                    <input placeholder="Título" type="text" onInput={(e) => {
-                        onHandleChangeTitle(e.target.value);
-                    }} value={taskTitle} />
-                    <br />
-                    <input placeholder="Descripción" type="text" onInput={(e) => {
-                        onHandleChangeDescription(e.target.value);
-                    }} value={taskDescription} />
-                    <br />
-                    <button type="submit" style={{alignSelf: "center"}}>+</button>
-                </form>
-            </div></>
-    );
-};
+export default function Header() {
+  return (
+    <Box
+      h={{ xsm: "60vh", md: "40vh" }}
+      bgImage={`url("https://img.freepik.com/foto-gratis/motociclista-silueta-conquista-pico-montana-al-anochecer-generado-ia_188544-40679.jpg?w=1060&t=st=1701223524~exp=1701224124~hmac=269faa1aa2ebf9cda3bb6e7aeb30cd4d1623ce7f11e94989e4fa6b696c0c9ae0")`}
+      backgroundPosition="center"
+      bgRepeat="no-repeat"
+      bgSize="cover"
+    >
+      <Flex
+        direction="column"
+        alignItems="center"
+        justify="center"
+        h="100%"
+        p={["0 10%", null, "0 20%"]}
+      >
+        <Heading
+          color="white"
+          textAlign="center"
+          fontWeight="light"
+          letterSpacing="5px"
+        >
+          DOMINA EL TERRENO
+        </Heading>
+
+        <Stack
+          direction={{ xsm: "column", md: "row" }}
+          spacing="40px"
+          mt="30px"
+          w={["100%", null, "auto"]}
+        >
+          <Button
+            variant="outline"
+            margin="2px"
+            size="lg"
+            fontWeight="light"
+            borderRadius="0px"
+            color="telegram"
+            letterSpacing="2px"
+            _hover={{
+              color: "black",
+              bg: "white",
+            }}
+          >
+            VER DETALLES
+          </Button>
+
+          <Button
+            variant="outline"
+            size="lg"
+            margin="2px"
+            fontWeight="light"
+            borderRadius="0px"
+            letterSpacing="2px"
+            _hover={{
+              color: "black",
+              bg: "white",
+            }}
+          >
+            VER VIDEO
+          </Button>
+        </Stack>
+      </Flex>
+    </Box>
+  );
+}
